b6e1016d25424ff65bfdcc0d48ece99be5f5342e
BRIJESH PRAJAPATI <94066180+BrijeshPrajapati8858@users.noreply.github.com>
2022-12-22 23:54:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 //route imports 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload({useTempFiles: true}));
